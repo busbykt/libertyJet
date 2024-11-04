971c67c59adc99aa96fc3e7ac7d8f3e2e7ed872c
kelton <busbykt@mail.uc.edu>
2024-11-04 07:12:44 -0800

    getdata fixes for writing to database

--- getData.py
+++ getData.py
@@ -7,25 +7,26 @@ import pandas as pd
 pd.set_option('display.max_columns', 50)
 
 # %%
-liberty_jet_db = sqlite3.connect("libertyJet.db")
-
-# %%
 while True:
     for aircraft in ['N270LE','N801PR', 'N45JE']:
-        result = requests.get(
-            f'https://opendata.adsb.fi/api/v2/callsign/{aircraft}'
-        )
+        with requests.Session() as s:
+            result = s.get(
+                f'https://opendata.adsb.fi/api/v2/callsign/{aircraft}'
+            )
         if result.status_code == 200:
-            print(f'found {aircraft}')
             result_json = result.json()
 
-            df = pd.DataFrame(result_json['ac'])
-            df.drop(
-                ['nav_modes','mlat','tisb'],
-                axis=1,
-                inplace=True,
-                errors='ignore'
-            )
-            df['now'] = result_json['now']
-            df.to_sql('adsb', con=liberty_jet_db, if_exists='append')
+            if result_json['ac']:
+                liberty_jet_db = sqlite3.connect("libertyJet.db")
+
+                df = pd.DataFrame(result_json['ac'])
+                df.drop(
+                    ['nav_modes','mlat','tisb'],
+                    axis=1,
+                    inplace=True,
+                    errors='ignore'
+                )
+                df['now'] = result_json['now']
+                df.to_sql('adsb', con=liberty_jet_db, if_exists='append')
+                liberty_jet_db.close()
         sleep(60)
